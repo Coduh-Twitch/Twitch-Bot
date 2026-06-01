9dbff3b185d13caff8ace0dd1cb3a2839dd35698
ducky <asqrybot@gmail.com>
2026-06-01 18:25:39 -0400

    fix: fine, you get a response when setting timer

--- src/commands/TimerCommand.ts
+++ src/commands/TimerCommand.ts
@@ -67,21 +67,21 @@ const TimerCommand: ChatCommand = {
         let args = content.split(/ +/);
 
         console.log("args", args)
-        
+
         let subcommand = args[0].toLowerCase();
         let subcommandData = TimerCommand.subCommands.find(sc => sc.name.toLowerCase() === subcommand);
         let authority = userHasAuthority(message.userInfo);
-        if(!authority || !subcommand || !subcommandData){
-            
-            
+        if (!authority || !subcommand || !subcommandData) {
+
+
             // !timer?
             let timer = getTimer();
             let formattedDuration = moment.duration(timer.seconds, "seconds").format(`${timer.seconds >= 86400 ? "DD:" : ""}${timer.seconds >= 3600 ? "HH:" : ""}${timer.seconds >= 60 ? "mm:" : "[00:]"}ss`);
 
             await reply(client, user, `⏱️ ${timer.paused ? `The timer is paused!${timer.seconds === 0 ? "" : ` There is ${formattedDuration} remaining.`}` : timer.seconds === 0 ? "The timer is not running!" : `The timer has ${formattedDuration} remaining!`}`, message)
-        } else if(subcommand && subcommandData) {
+        } else if (subcommand && subcommandData) {
 
-            if(subcommand === "set") {
+            if (subcommand === "set") {
                 try {
                     let duration = args?.[1];
                     let durationUnit = duration ? duration.substring(duration.length - 1) : null;
@@ -90,20 +90,23 @@ const TimerCommand: ChatCommand = {
                     console.log(duration)
                     console.log(durationUnit)
                     console.log(duration_seconds)
-                    if(!duration || duration_seconds === 0) return reply(client, user, `Correct Usage: !${TimerCommand.name} ${subcommand} {duration} (i.e. !${TimerCommand.name} ${subcommand} 20m)`, message);
-                    if(duration_seconds >= 2505600) return reply(client, user, `Duration must be less than 29 days`, message);
+                    if (!duration || duration_seconds === 0) return reply(client, user, `Correct Usage: !${TimerCommand.name} ${subcommand} {duration} (i.e. !${TimerCommand.name} ${subcommand} 20m)`, message);
+                    if (duration_seconds >= 2505600) return reply(client, user, `Duration must be less than 29 days`, message);
 
                     let newTimer = setTimerSeconds(duration_seconds);
-                    if(!newTimer.visible) setTimerVisibility(true);
-                    
-                } catch(e) {
+                    if (!newTimer.visible) setTimerVisibility(true);
+
+                    let formattedDuration = moment.duration(newTimer.seconds, "seconds").format(`${newTimer.seconds >= 86400 ? "DD:" : ""}${newTimer.seconds >= 3600 ? "HH:" : ""}${newTimer.seconds >= 60 ? "mm:" : "[00:]"}ss`);
+
+                    reply(client, user, `⏱️ Set the timer to ${formattedDuration}`, message)
+                } catch (e) {
                     return reply(client, user, `Correct Usage: !${TimerCommand.name} ${subcommand} {duration} (i.e. !${TimerCommand.name} ${subcommand} 20m)`, message);
                 }
             }
 
-            if(subcommand === "show") {
+            if (subcommand === "show") {
                 let timer = getTimer();
-                if(timer.visible) return reply(client, user, `The timer is already visible!`, message);
+                if (timer.visible) return reply(client, user, `The timer is already visible!`, message);
                 try {
                     setTimerVisibility(true);
                     reply(client, user, `Showing the timer!`, message)
@@ -112,9 +115,9 @@ const TimerCommand: ChatCommand = {
                 }
             }
 
-            if(subcommand === "hide") {
+            if (subcommand === "hide") {
                 let timer = getTimer();
-                if(!timer.visible) return reply(client, user, `The timer is already hidden!`, message);
+                if (!timer.visible) return reply(client, user, `The timer is already hidden!`, message);
                 try {
                     setTimerVisibility(false);
                     reply(client, user, `Hiding the timer!`, message)
@@ -123,9 +126,9 @@ const TimerCommand: ChatCommand = {
                 }
             }
 
-            if(subcommand === "pause") {
+            if (subcommand === "pause") {
                 let timer = getTimer();
-                if(timer.paused) return reply(client, user, `The timer is already paused!`, message);
+                if (timer.paused) return reply(client, user, `The timer is already paused!`, message);
                 try {
                     setTimerPaused(true);
                     reply(client, user, `Paused the timer!`, message)
@@ -134,9 +137,9 @@ const TimerCommand: ChatCommand = {
                 }
             }
 
-            if(subcommand === "unpause") {
+            if (subcommand === "unpause") {
                 let timer = getTimer();
-                if(!timer.paused) return reply(client, user, `The timer is not paused!`, message);
+                if (!timer.paused) return reply(client, user, `The timer is not paused!`, message);
                 try {
                     setTimerPaused(false);
                     reply(client, user, `Unpaused the timer!`, message)
@@ -145,12 +148,12 @@ const TimerCommand: ChatCommand = {
                 }
             }
 
-            if(subcommand === "label") {
+            if (subcommand === "label") {
                 args.shift();
                 let label = args.join(" ").trim();
-                if(!label || label === "") return reply(client, user, `!${TimerCommand.name} ${subcommand} {new_label}`, message)
-                if(label.toLowerCase() === "default") label = "Active Timer";
-                if(label.toLowerCase() === "hide") {
+                if (!label || label === "") return reply(client, user, `!${TimerCommand.name} ${subcommand} {new_label}`, message)
+                if (label.toLowerCase() === "default") label = "Active Timer";
+                if (label.toLowerCase() === "hide") {
                     setTimerLabelVisibility(false);
                     return reply(client, user, `Hiding the timer label`, message)
                 } else {
