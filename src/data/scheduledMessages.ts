1d3bb65b0e0e0d6401890bce21add5ebfc469b12
ducky <asqrybot@gmail.com>
2026-06-22 18:16:49 -0400

    fix: title check for room code scheduled message

--- src/data/scheduledMessages.ts
+++ src/data/scheduledMessages.ts
@@ -2,9 +2,9 @@ import { apiClient } from "..";
 import { getDiscordCta, getRoomCode } from "../util";
 
 interface ScheduledMessage {
-    id: string;
-    enabled: true;
-    getContent: (() => Promise<string | null>) | (() => string | null);
+  id: string;
+  enabled: true;
+  getContent: (() => Promise<string | null>) | (() => string | null);
 }
 
 let secs = 0;
@@ -12,35 +12,45 @@ let secs = 0;
 export const scheduledMessageTimeout = 120e3;
 
 const scheduledMessages: ScheduledMessage[] = [
-    {
-        id: "discord",
-        enabled: true,
-        getContent: async () => {
-            return await getDiscordCta();
-        }
+  {
+    id: "discord",
+    enabled: true,
+    getContent: async () => {
+      return await getDiscordCta();
     },
-    {
-        id: "follow",
-        enabled: true,
-        getContent: () => `If you're enjoying the stream, don't forget to follow!`
+  },
+  {
+    id: "follow",
+    enabled: true,
+    getContent: () => `If you're enjoying the stream, don't forget to follow!`,
+  },
+  {
+    id: "game-code",
+    enabled: true,
+    getContent: async () => {
+      let stream = await apiClient.streams.getStreamByUserName(
+        process.env.CHANNEL,
+      );
+      // if (!stream) return null;
+      let game: string = stream?.gameName || "NOSTREAM";
+      let code = await getRoomCode();
+      // game = "Jackbox Party Packs"
+      if (code && !code.toLowerCase().includes("hidden")) {
+        if (
+          game.includes("Mario Kart World") &&
+          stream.title.includes("!code")
+        ) {
+          return `Join the open lobby! | Online -> Friends -> Enter Room Code -> ${await getRoomCode()}`;
+        } else if (
+          game.includes("Jackbox") &&
+          stream.title.includes("!jackbox") &&
+          code.trim().length <= 4
+        ) {
+          return `Join Jackbox! @ https://jackbox.tv -> ${await getRoomCode()}`;
+        } else return null;
+      } else return null;
     },
-    {
-        id: "game-code",
-        enabled: true,
-        getContent: async () => {
-            let stream = await apiClient.streams.getStreamByUserName(process.env.CHANNEL);
-            // if (!stream) return null;
-            let game: string = stream?.gameName || "NOSTREAM";
-            // game = "Jackbox Party Packs"
-            if (await getRoomCode()) {
-                if (game.includes("Mario Kart World") || game === "Nothing") {
-                    return `Join the open lobby! | Online -> Friends -> Enter Room Code -> ${await getRoomCode()}`;
-                } else if(game.includes("Jackbox")) {
-                    return `Join Jackbox! @ https://jackbox.tv -> ${await getRoomCode()}`
-                } else return null;
-            } else return null;
-        }
-    }
-]
+  },
+];
 
 export default scheduledMessages;
